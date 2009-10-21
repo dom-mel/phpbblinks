f7d9762c606cdbedd37aa830f211cb53296620ad
Dominik Eckelmann <deckelmann@gmail.com>
2009-10-21 11:53:06 +0200

    changed some localisation stuff

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['integration'] = 'Use the php integration mode';
+$lang['integration'] = 'Use the phpBB integration mode';
 $lang['enable']      = 'Enable the url replacement';
 $lang['boardurl']    = 'The url to the Board to replace links';
 $lang['host']        = 'The MySQL Database Server from phpbb';
